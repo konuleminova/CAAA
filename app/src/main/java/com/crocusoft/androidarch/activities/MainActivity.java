7ef349b57fb75aa11d49e0c040145152a62c6fc4
konuleminova <konul.eminova39@gmail.com>
2018-02-13 12:06:34 +0400

    created CAAA project

---
+++ app/src/main/java/com/crocusoft/androidarch/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crocusoft.androidarch.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crocusoft.androidarch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
