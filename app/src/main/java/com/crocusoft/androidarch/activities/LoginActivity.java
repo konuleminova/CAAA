172c9a5ea41ed11e69d7fe01f8cd47f1c951da7d
konuleminova <konul.eminova39@gmail.com>
2018-02-13 20:13:44 +0400

    fixed

--- app/src/main/java/com/crocusoft/androidarch/activities/LoginActivity.java
+++ app/src/main/java/com/crocusoft/androidarch/activities/LoginActivity.java
@@ -1,30 +1,33 @@
 package com.crocusoft.androidarch.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
 import com.crocusoft.androidarch.R;
 import com.crocusoft.androidarch.api.ApiClient;
 import com.crocusoft.androidarch.api.ApiInterface;
 import com.crocusoft.androidarch.model.LoginRequest;
 import com.crocusoft.androidarch.model.LoginResponse;
-
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btnOk;
-
+    Button btnSave;
+    SharedPreferences sharedPreferences;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnOk = (Button) findViewById(R.id.btnOk);
-        btnOk.setOnClickListener(new View.OnClickListener() {
+        sharedPreferences = getSharedPreferences("mypreference", Context.MODE_PRIVATE);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 login();
@@ -41,9 +44,9 @@ public class LoginActivity extends AppCompatActivity {
         apiInterface.login(loginRequest).enqueue(new Callback<LoginResponse>() {
             @Override
             public void onResponse(retrofit2.Call<LoginResponse> call, Response<LoginResponse> response) {
-                String getMessageId=String.valueOf(response.body().getMessageId());
+                String getMessageId = String.valueOf(response.body().getMessageId());
                 Toast.makeText(getBaseContext(), getMessageId, Toast.LENGTH_LONG).show();
-                Log.e("konul",response.body().toString());
+                Log.e("konul", response.body().toString());
             }
 
             @Override
